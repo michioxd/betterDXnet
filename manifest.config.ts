315ca61f40cf45dd674a46a70b1b736e43825028
michioxd <michio.haiyaku@gmail.com>
2026-07-14 23:09:34 +0700

    icon

--- manifest.config.ts
+++ manifest.config.ts
@@ -7,7 +7,10 @@ export default defineManifest({
     description: "just an alternative UI for sinmaiDX :)",
     version: pkg.version,
     icons: {
-        48: "public/logo.png",
+        16: "assets/16.png",
+        32: "assets/32.png",
+        48: "assets/48.png",
+        128: "assets/128.png",
     },
     permissions: ["contentSettings"],
     content_scripts: [
